c464bfcf0d5b8932268a27423a489bfb78710e25
DGC20 <dgc20@pitt.edu>
2019-11-13 00:35:27 -0500

    commit

--- PersonalWebsiteDGC/App_Start/RouteConfig.cs
+++ PersonalWebsiteDGC/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace PersonalWebsiteDGC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //hi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
